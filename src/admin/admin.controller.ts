41cf8089ff346f1238bfb17282b3ce19cab416fd
Dandi Rizky <dandirizkyy94@gmail.com>
2023-05-30 20:43:08 +0700

    guarding admins endpoint

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,22 +1,35 @@
-import { Controller, Delete, Get, Param, ParseIntPipe, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { AdminService } from './admin.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AuthGuard, RoleGuard } from 'src/auth/guard/auth.guard';
 
 @Controller('admins')
 @ApiTags('admins')
 export class AdminController {
+  constructor(private adminService: AdminService) {}
 
-    constructor(private adminService: AdminService) {}
+  // get all admins
+  @UseGuards(RoleGuard)
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @Get()
+  async getAllAdmins() {
+    return this.adminService.getAllAdmins();
+  }
 
-    // get all admins
-    @Get()
-    async getAllAdmins() {
-        return this.adminService.getAllAdmins();
-    }
-
-    // delete admin
-    @Delete(':id')
-    async deleteAdmin(@Param('id', ParseIntPipe) id: number) {
-        return this.adminService.deleteAdmin(id);
-    }
+  // delete admin
+  @UseGuards(RoleGuard)
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @Delete(':id')
+  async deleteAdmin(@Param('id', ParseIntPipe) id: number) {
+    return this.adminService.deleteAdmin(id);
+  }
 }
