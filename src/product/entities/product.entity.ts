4b1886c98d78b862454551bc41dfff40c90933e0
Dandi Rizky <dandirizkyy94@gmail.com>
2023-05-31 13:42:46 +0700

    add product entity for response header

---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Product } from '@prisma/client';
+import { Decimal } from '@prisma/client/runtime';
+
+export class ProductEntity implements Product {
+  @ApiProperty({ default: 1 })
+  id: number;
+
+  @ApiProperty({ default: 'Example Title' })
+  title: string;
+
+  @ApiProperty({ default: 'Example Description' })
+  description: string;
+
+  @ApiProperty({ default: 1000 })
+  price: Decimal;
+
+  @ApiProperty({ default: 'Example Categories' })
+  categories: string;
+
+  @ApiProperty({ required: false, default: 1 })
+  wishlistId: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
