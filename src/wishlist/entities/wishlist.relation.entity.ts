d5465e39d3b2a00d0d8e2d60ce0120edabc40350
Dandi Rizky <dandirizkyy94@gmail.com>
2023-06-01 09:44:42 +0700

    adding relation entity on wishlist

---
+++ src/wishlist/entities/wishlist.relation.entity.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Wishlist } from '@prisma/client';
+import { ProductEntity } from 'src/product/entities/product.entity';
+
+export class WishlistRelationEntity implements Wishlist {
+  @ApiProperty({ default: 1 })
+  id: number;
+
+  @ApiProperty({ default: 'Example Name' })
+  name: string;
+
+  @ApiProperty({ default: 1 })
+  userId: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty({ required: false, type: ProductEntity })
+  products?: ProductEntity;
+}
